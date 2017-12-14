70e6cb1e3fcbd0cc781fe9bfd4005d3aead069e1
adeeburrahman <23088430+adeeburrahman@users.noreply.github.com>
2017-12-13 19:40:36 -0500

    minor modifications

--- find.js
+++ find.js
@@ -13,17 +13,17 @@ var distance = function (x0, y0, x1, y1) {
     return ((x1-x0)**2+(y1-y0)**2)**.5;
 };
 
-var dist = 0, maxDist = 0, percentage = 0;
+var dist = 0, maxDist = 0, percentage = 0, threshold = 0.03;
 
 var findIt = function(e) {
   dist = distance(e.x, e.y, targetX, targetY);
   maxDist = distance(0, 0, boxWidth, boxHeight) / 2;
   percentage = dist/maxDist;
-  percentage > .05 ? box.setAttribute("style", "background-color: rgba(0,0,125," + percentage +  ")") : box.setAttribute("style", "cursor: pointer; background-color: rgba(0,0,125," + percentage +  ")");
+  percentage > threshold ? box.setAttribute("style", "background-color: rgba(0,0,125," + percentage + ")") : box.setAttribute("style", "cursor: pointer; background-color: rgba(0,0,125," + percentage + ")");
 };
 
 var foundIt = function(e) {
-  if (percentage < .05) alert("You found the cow!");
+  if (percentage < threshold) alert("You found the cow!");
 };
 
 box.addEventListener("mousemove", findIt);
